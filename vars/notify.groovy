fdc7ede43273db692b9df0f279e6c96ab8c99e38
Alexander Suter <alexander.suter@axonivy.com>
2018-08-30 09:34:26 +0200

    Improve success message

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,7 +1,7 @@
 def call(currentBuild) {
   String buildResult = currentBuild.currentResult;
   if (buildResult == "SUCCESS") {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+    slackSend color: "good", message: "${env.JOB_NAME} | #${env.BUILD_NUMBER} | ${currentBuild.durationString} | ${currentBuild.absoluteUrl}"
   }
   else if (buildResult == "FAILURE") { 
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
