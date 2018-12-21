a0aa27de7d2b37827c8319dcaf8581f8e6953351
Alexander Suter <alexander.suter@axonivy.com>
2018-12-21 12:59:44 +0000

    Use specific timezone Australia/Melbourne

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,5 +1,5 @@
 def call(Map config) {
 	configFileProvider([configFile(fileId: 'global-maven-settings', variable: 'GLOBAL_MAVEN_SETTINGS')]) {
-		sh "mvn --batch-mode -gs $GLOBAL_MAVEN_SETTINGS -e ${config.cmd} "
+		sh "mvn --batch-mode -gs $GLOBAL_MAVEN_SETTINGS -e -Duser.timezone=Australia/Melbourne ${config.cmd} "
 	}
 }
