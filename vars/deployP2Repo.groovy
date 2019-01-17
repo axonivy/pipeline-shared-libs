42b6cb84849adbeea23826eb59a2df30ad761153
Alexander Suter <alexander.suter@axonivy.com>
2019-01-17 19:13:20 +0100

    Add deployP2Repo

--- vars/deployP2Repo.groovy
+++ vars/deployP2Repo.groovy
@@ -1,24 +1,21 @@
+// deploy a p2 directory to our p2 hosting infrastructure
+// param sourceDir: source directory in your workspace
+// param destDir: destination directory on server
+// example: deployP2Repo sourceDir: 'target/repository/' destDir: 'nightly'
+
 def call(Map config) {
-  stage('deploy-p2-repo') {
-    agent { 
-      docker 'maven:3-alpine'
-      reuseNode true
-    } 
-    steps {
-      echo 'im running on the same node and workspace!'
-    }
+  def destDir = config.destDir
+  if (destDir == '') {
+    throw new Exception("destDir is empty")
+  }
+
+  sshagent(['zugprojenkins-ssh']) {
+    def host = 'axonivy1@217.26.54.241'
+    def destFolder = "/home/axonivy1/data/p2/${destDir}"
+
+    echo "Upload p2 repository to $host:$destFolder"
+    sh "ssh $host mkdir -p $destFolder"
+    sh "rsync -r ${config.sourceDir} $host:$destFolder"
+    sh "ssh $host touch $destFolder/p2.ready"
   }
 }
-
-  //sshagent(['zugprojenkins-ssh']) {
-  //  def host = 'axonivy1@217.26.54.241'
-  //  def destFolder = "/home/axonivy1/data/p2/${config.destDir}"
-
-  ///  echo "Upload p2 repository to $host:$destFolder"
-  //  sh "ssh $host mkdir -p $destFolder"
-  //  sh "rsync -r ${config.sourceDir} $host:$destFolder"
-  //  sh "ssh $host touch $destFolder/p2.ready"
-  //}
-
-
-//deployP2Repo sourceDir: 'target/repository/' destDir: 'nightly' 
