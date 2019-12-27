6f94dacdb31d75d354a8789f764c4aac6539bf51
Alexander Suter <alexander.suter@axonivy.com>
2019-12-27 09:02:13 +0000

    Run maven on windows node as bat command
    - pull request #3
    
    Approved-by: Christian Strebel

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,5 +1,10 @@
 def call(Map config) {
-	configFileProvider([configFile(fileId: 'global-maven-settings', variable: 'GLOBAL_MAVEN_SETTINGS')]) {
-		sh "mvn --batch-mode -gs $GLOBAL_MAVEN_SETTINGS -e -Duser.timezone=Europe/Zurich ${config.cmd} "
-	}
+  configFileProvider([configFile(fileId: 'global-maven-settings', variable: 'GLOBAL_MAVEN_SETTINGS')]) {
+    def mavenCommand = "mvn --batch-mode -gs $GLOBAL_MAVEN_SETTINGS -e -Duser.timezone=Europe/Zurich ${config.cmd} "
+    if (isUnix()) {
+      sh mavenCommand
+    } else {
+      bat mavenCommand
+    }
+  }
 }
