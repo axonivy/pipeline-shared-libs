b6fde1b083a60dd66a8a74497981b0e43c98087d
Alexander Suter <alexander.suter@axonivy.com>
2020-02-17 09:10:27 +0100

    Isolate .m2 repository in windows builds

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,10 +1,10 @@
 def call(Map config) {
   configFileProvider([configFile(fileId: 'global-maven-settings', variable: 'GLOBAL_MAVEN_SETTINGS')]) {
-    def mavenCommand = "mvn --batch-mode -gs $GLOBAL_MAVEN_SETTINGS -e -Duser.timezone=Europe/Zurich ${config.cmd} "
+    def mavenCommand = "mvn --batch-mode -gs $GLOBAL_MAVEN_SETTINGS -e -Duser.timezone=Europe/Zurich "
     if (isUnix()) {
-      sh mavenCommand
+      sh mavenCommand + config.cmd
     } else {
-      bat mavenCommand + " -Dmaven.repo.local=${WORKSPACE}/.m2-repo"
+      bat mavenCommand + "-Dmaven.repo.local=${WORKSPACE}/.m2-repo " + config.cmd
     }
   }
 }
