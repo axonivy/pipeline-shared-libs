a7fb03ceb511437ea1695ca718dbe07fc8406e38
Oliver Heer <oliver.heer@axonivy.com>
2020-04-06 08:12:45 +0000

    Merged in feature/XIVY-3774_checkVersions (pull request #5)
    Feature/XIVY-3774 checkVersions
    
    * XIVY-3774 Add new call checkVersions to check if plugins & dependencies are up to date
    
    Approved-by: Alexander Suter

---
+++ vars/checkVersions.groovy
@@ -0,0 +1,16 @@
+def call(Map config) {
+  def versionsLog = "versions.log"
+  def mavenVersionCheckPlugin = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-plugin-updates "
+  def mavenVersionCheckDependency = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-dependency-updates "
+
+  def cmd = config.get("cmd", "")
+  def recordIssue = config.get("recordIssue", true)
+
+  maven cmd: cmd + mavenVersionCheckPlugin + mavenVersionCheckDependency + " >> " + versionsLog
+
+  if (recordIssue) {
+    recordIssues tools: [groovyScript(parserId: 'maven-version-update-parser', pattern: versionsLog)], unstableTotalAll: 1
+  }
+
+  archiveArtifacts versionsLog
+}
