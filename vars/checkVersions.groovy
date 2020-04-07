5052178c68c8b26e74825ceae3724853cebdc7ee
Oliver Heer <oliver.heer@axonivy.com>
2020-04-07 05:30:12 +0000

    Merged in feature/XIVY-3774_qualityGate (pull request #7)
    XIVY-3774 checkVersions use recordIssues with unstableNewAll
    
    * XIVY-3774 Make check for plugins & dependencies configurable
    
    Approved-by: Alexander Suter

--- vars/checkVersions.groovy
+++ vars/checkVersions.groovy
@@ -1,10 +1,20 @@
 def call(Map config = [:]) {
   def versionsLog = "versions.log"
-  def mavenVersionCheckPlugin = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-plugin-updates "
-  def mavenVersionCheckDependency = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-dependency-updates "
+  def mavenVersionCheckPlugin = " "
+  def mavenVersionCheckDependency = " "
 
   def cmd = config.get("cmd", "")
   def recordIssue = config.get("recordIssue", true)
+  def checkPlugins = config.get("checkPlugins", true)
+  def checkDependencies = config.get("checkDependencies", false)
+
+  if (checkPlugins) {
+    mavenVersionCheckPlugin = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-plugin-updates "
+  }
+
+  if (checkDependencies) {
+    mavenVersionCheckDependency = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-dependency-updates "
+  }
 
   maven cmd: cmd + mavenVersionCheckPlugin + mavenVersionCheckDependency + " >> " + versionsLog
 
