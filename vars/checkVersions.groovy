c889c0e7e05b0117247db9670b47ad643df5ed1a
Oliver Heer <oliver.heer@axonivy.com>
2020-04-08 13:36:04 +0000

    Merged in feature/XIVY-3774_versionRules (pull request #8)
    Feature/XIVY-3774 versionRules
    
    * XIVY-3774 Enable checking only for project-build-plugins
    * XIVY-3774 Make the project-build-plugin to check for configurable
    
    Approved-by: Alexander Suter

--- vars/checkVersions.groovy
+++ vars/checkVersions.groovy
@@ -7,13 +7,17 @@ def call(Map config = [:]) {
   def recordIssue = config.get("recordIssue", true)
   def checkPlugins = config.get("checkPlugins", true)
   def checkDependencies = config.get("checkDependencies", false)
+  def onlyProjectBuildPluginWithVersion = config.get("onlyProjectBuildPluginWithVersion", "")
+  def additionalVersionArgs = config.get("additionalVersionArgs", "")
+
+  def mavenVersionArguments = addVersionRules(onlyProjectBuildPluginWithVersion) + additionalVersionArgs
 
   if (checkPlugins) {
-    mavenVersionCheckPlugin = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-plugin-updates "
+    mavenVersionCheckPlugin = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-plugin-updates " + mavenVersionArguments
   }
 
   if (checkDependencies) {
-    mavenVersionCheckDependency = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-dependency-updates "
+    mavenVersionCheckDependency = " org.codehaus.mojo:versions-maven-plugin:RELEASE:display-dependency-updates " + mavenVersionArguments
   }
 
   maven cmd: cmd + mavenVersionCheckPlugin + mavenVersionCheckDependency + " >> " + versionsLog
@@ -23,4 +27,17 @@ def call(Map config = [:]) {
   }
 
   archiveArtifacts versionsLog
+}
+
+def addVersionRules(String onlyProjectBuildPluginWithVersion)
+{
+  def versionRules = " "
+  if (onlyProjectBuildPluginWithVersion) {
+    def rulesFileName = "version-rules.xml"
+    def versionRulesXML = libraryResource rulesFileName
+    versionRulesXML = versionRulesXML.replace("{version}", onlyProjectBuildPluginWithVersion)
+    writeFile(file: rulesFileName, text: versionRulesXML, encoding: "UTF-8")
+    versionRules = " -Dmaven.version.rules=file://" + "${env.WORKSPACE}/" + rulesFileName + " "
+  }
+  return versionRules
 }
